4dca866187fef5b38e75d98cd917d3d8aa20325d
Eda31 <jademarcaillou@gmail.com>
2023-10-24 17:04:45 +0200

    Phase 24 : tests (compléments).

--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -2,7 +2,9 @@
 
 namespace App\Tests;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -13,7 +15,22 @@ use PHPUnit\Framework\TestCase;
 class VisiteTest extends TestCase {
     public function testGetDatecreationString() {
         $visite = new Visite();
-        $visite->setDatecreation(new \DateTime("2022-04-14"));
+        $visite->setDatecreation(new DateTime("2022-04-14"));
         $this->assertEquals("14/04/2022", $visite->getDatecreationString());
+    }
+    
+    public function testAjoutEnvironnementDejaPresent() {
+        $visite = new Visite();
+        // Crée un environnement
+        $environnement = new Environnement();
+        $environnement->setNom('Montagne');
+        // Ajoute l'environnement à la visite
+        $visite->addEnvironnement($environnement);
+        // Vérifie que l'environnement est présent dans la visite
+        $this->assertContains($environnement, $visite->getEnvironnements());
+        // Ajoute le même environnement à la visite à nouveau
+        $visite->addEnvironnement($environnement);
+        // Vérifie que l'environnement n'a pas été ajouté une deuxième fois
+        $this->assertCount(1, $visite->getEnvironnements());
     }
 }
